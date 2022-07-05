2307c7f29510f44d410e58b7cd7f33893bf2c9f1
Raymondu1996@outlook.com <Raymondu1996@outlook.com>
2022-07-05 14:44:27 +0800

    Number guess game

---
+++ notes/PlayerGuessNumber/GameLauncher.java
@@ -0,0 +1,9 @@
+package PlayerGuessNumber;
+
+public class GameLauncher {
+    
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    } 
+}
